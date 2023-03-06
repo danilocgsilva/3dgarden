86831a4409fce92b5be10f7f6e48f54c8b5361d8
Danilo Carlos de Góes Silva <danilocgsilva@gmail.com>
2023-03-06 08:58:09 -0300

    Studing the position generation equation

--- gravity_simulation/generate_positions.py
+++ gravity_simulation/generate_positions.py
@@ -1,9 +1,16 @@
 def calculate_decay_value(distance):
-    return 1 / pow(2, distance)
+    return 1 / pow(distance, 2)
 
-vector_speed = 0
-position = 10
-for i in range(1000):
+def motor(position, iteration):
     decay_value = calculate_decay_value(position)
-    next_vector_speed = vector_speed * decay_value
-    print(str(i) + ", 0")
+    # next_vector_speed = 1 * decay_value
+    position = position - decay_value
+    print("iteration: " + str(iteration) + ", position: " + str(position) + ", decay value: " + str(decay_value))
+    return position
+
+# vector_speed = 0
+# position = 10
+# for i in range(3000):
+#     position = motor(position, i)
+
+motor(0.21481365662823804, 0)
