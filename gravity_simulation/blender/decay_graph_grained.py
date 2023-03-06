6ece6ee3e35f035d2b07ca0809a9a5ff0ec26277
Danilo Carlos de Góes Silva <danilocgsilva@gmail.com>
2023-03-06 08:30:04 -0300

    Improvements in blender decay equations

--- gravity_simulation/blender/decay_graph_grained.py
+++ gravity_simulation/blender/decay_graph_grained.py
@@ -6,17 +6,18 @@ curve_object.dimensions = "3D"
 
 line_height = 1
 points = []
-for i in range(1,10,1):
-    decay_power =  pow(i, 2)
+for i in range(2,20,1):
+    distance = i / 2
+    decay_power =  pow(distance, 2)
     points.append(line_height / decay_power)
     if print_decay:
-        print('Decay power: ' + str(decay_power) + ', distance: ' + str(i))
+        print('Decay power: ' + str(decay_power) + ', distance: ' + str(distance))
 
 spline = curve_object.splines.new(type="POLY")
 spline.points.add(len(points)-1)
 
 for pointLoop in range(len(points)):
-    spline.points[pointLoop].co = (0, pointLoop, points[pointLoop], 1)
+    spline.points[pointLoop].co = (0, pointLoop / 2, points[pointLoop], 1)
     
 curve_obj = bpy.data.objects.new("graph", curve_object)
 bpy.context.scene.collection.objects.link(curve_obj)
