0566ce8cc0cb00104fb10646d228b58f5e9a3fe3
Danilo Carlos de Góes Silva <danilocgsilva@gmail.com>
2023-03-10 07:59:44 -0300

    Animating by positions
    Added files to generate z postions and also animate based on then.
    Also left a sequence file as example

--- gravity_simulation/generate_positions.py
+++ gravity_simulation/generate_positions.py
@@ -1,14 +1,8 @@
-def calculate_decay_value(distance):
-    return 1 / pow(distance, 2)
+inertia = -0.4
+position = 0
+force = 0.001
 
-def motor(position, iteration):
-    decay_value = calculate_decay_value(position)
-    new_position = position - decay_value
-    return position, new_position, decay_value
-
-vector_speed = 0
-position = 5
-for i in range(100):
-    old_position, position, decay_value = motor(position, i)
-    print("old position: {0}, position: {1}, decay value: {2}".format(str(old_position), str(position), str(decay_value)))
-
+for i in range(1000):
+    inertia += force
+    position = position + inertia
+    print(str(position))
